50cd23bc213d7d343be1c3633eb83f658e563a13
Uiliam Santos <uiliam.santos@litoralverde.com>
2024-09-05 17:10:49 -0300

    criando html

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 # from django.shortcuts import render
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
+    return HttpResponse('do arquivo views.py')
+    return render(request, 'recipes/home.html')
 
 
 def contato(request):
     return HttpResponse('contato')
-
-
 def sobre(request):
     return HttpResponse('sobre')
