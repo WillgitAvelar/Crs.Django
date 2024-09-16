f12276194b85bfea4f8f5d3ebfba113b81b88046
Uiliam Santos <uiliam.santos@litoralverde.com>
2024-09-16 18:29:41 -0300

    fim da 55

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,5 @@
 """projeto URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -12,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include  # Aqui estamos importando o include
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),  # Aqui você está utilizando o include para adicionar as URLs do app 'recipes'
+    path('', include('recipes.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
